c0ccfc75a008b5aa422d968f1798246d0438f9ad
Ragul M <ragul.m@optisolbusiness.com>
2022-06-28 22:17:10 +0530

    Initial Commit

---
+++ OptiEmailValidator/OptiEmailValidator.h
@@ -0,0 +1,18 @@
+//
+//  OptiEmailValidator.h
+//  OptiEmailValidator
+//
+//  Created by Mac-OBS-18 on 28/06/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OptiEmailValidator.
+FOUNDATION_EXPORT double OptiEmailValidatorVersionNumber;
+
+//! Project version string for OptiEmailValidator.
+FOUNDATION_EXPORT const unsigned char OptiEmailValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OptiEmailValidator/PublicHeader.h>
+
+
