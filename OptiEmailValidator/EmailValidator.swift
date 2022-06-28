01615c228430eb01ac14f006e4a1d2f74fe8cae5
Ragul M <ragul.m@optisolbusiness.com>
2022-06-28 23:18:52 +0530

    Added Alamofire framework

--- OptiEmailValidator/EmailValidator.swift
+++ OptiEmailValidator/EmailValidator.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import Alamofire
+
 public struct EmailValidator{
     public static func emailValidator(_ email: String) -> Bool{
     let emailChecker = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
