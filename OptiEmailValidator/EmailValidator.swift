f91cf79a95c76dc00273c5a198f2d956b45fc76f
Ragul M <ragul.m@optisolbusiness.com>
2022-06-28 22:28:10 +0530

    first commit

---
+++ OptiEmailValidator/EmailValidator.swift
@@ -0,0 +1,18 @@
+//
+//  EmailValidator.swift
+//  OptiEmailValidator
+//
+//  Created by Mac-OBS-18 on 28/06/22.
+//
+
+import Foundation
+public struct Validator{
+    public static func emailValidator(_ email: String) -> Bool{
+    let emailChecker = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    let emailPredictor = NSPredicate(format: "SELF MATCHES %@", emailChecker)
+    return emailPredictor.evaluate(with: email)
+ }
+    public static func sayHello() {
+        print("Hey User!")
+    }
+}
